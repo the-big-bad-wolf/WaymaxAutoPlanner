0cf22e3924991c475e07278836015220685db758
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2025-05-15 03:24:10 +0200

    add SAC agent

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import dataclasses
 
 from skrl import config
 from skrl.agents.jax.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.agents.jax.sac import SAC, SAC_DEFAULT_CONFIG
 from skrl.memories.jax import RandomMemory
 from skrl.resources.schedulers.jax import KLAdaptiveRL
 from skrl.resources.preprocessors.jax import RunningStandardScaler
@@ -10,14 +11,14 @@ from waymax import agents
 from waymax import config as _config
 from waymax import dataloader, dynamics
 
-from models import Policy_Model, Value_Model
+from models import Policy_Model, Value_Model, Critic_Model
 from waymax_modified import WaymaxEnv
 from waymax_wrapper import WaymaxWrapper
 
 
 def setup_waymax():
     path = "gs://waymo_open_dataset_motion_v_1_3_0/uncompressed/tf_example/training/training_tfexample.tfrecord@1000"
-    path = "data/training_tfexample.tfrecord@5"
+    # path = "data/training_tfexample.tfrecord@5"
     max_num_objects = 128
     data_loader_config = dataclasses.replace(
         _config.WOD_1_1_0_TRAINING,
@@ -37,7 +38,7 @@ def setup_waymax():
         metrics_to_run=("sdc_progression", "offroad", "overlap")
     )
     reward_config = _config.LinearCombinationRewardConfig(
-        rewards={"sdc_progression": 1.0, "offroad": -20.0, "overlap": -20.0},
+        rewards={"sdc_progression": 1.0, "offroad": -1.0, "overlap": -2.0},
     )
     env_config = dataclasses.replace(
         _config.EnvironmentConfig(),
@@ -61,45 +62,81 @@ if __name__ == "__main__":
     config.jax.backend = "numpy"
 
     env, scenario_loader = setup_waymax()
-    env = WaymaxWrapper(env, scenario_loader, action_space_type="bicycle", MPC=False)
+    env = WaymaxWrapper(
+        env, scenario_loader, action_space_type="trajectory_sampling", MPC=False
+    )
 
     # instantiate a memory as rollout buffer
     mem_size = 16384
-    memory = RandomMemory(memory_size=mem_size, num_envs=1)
+    ppo_memory = RandomMemory(memory_size=mem_size, num_envs=1)
 
-    models = {}
-    models["policy"] = Policy_Model(env.observation_space, env.action_space)
-    models["value"] = Value_Model(env.observation_space, env.action_space)
-    for role, model in models.items():
+    ppo_models = {}
+    ppo_models["policy"] = Policy_Model(env.observation_space, env.action_space)
+    ppo_models["value"] = Value_Model(env.observation_space, env.action_space)
+    for role, model in ppo_models.items():
         model.init_state_dict(role)
 
-    cfg = PPO_DEFAULT_CONFIG.copy()
-    cfg["rollouts"] = mem_size  # memory_size
-    cfg["mini_batches"] = 128
-    cfg["learning_epochs"] = 8
-    cfg["entropy_loss_scale"] = 0.01
-    # cfg["learning_rate"] = 3e-4
-    cfg["learning_rate_scheduler"] = KLAdaptiveRL
-    cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
-    cfg["state_preprocessor"] = RunningStandardScaler
-    cfg["state_preprocessor_kwargs"] = {"size": env.observation_space}
-    cfg["value_preprocessor"] = RunningStandardScaler
-    cfg["value_preprocessor_kwargs"] = {"size": 1}
+    ppo_cfg = PPO_DEFAULT_CONFIG.copy()
+    ppo_cfg["rollouts"] = mem_size  # memory_size
+    ppo_cfg["mini_batches"] = 32
+    ppo_cfg["learning_epochs"] = 8
+    ppo_cfg["entropy_loss_scale"] = 0.005
+    ppo_cfg["ratio_clip"] = 0.2
+    # cfg["learning_rate"] = 1e-4
+    ppo_cfg["learning_rate_scheduler"] = KLAdaptiveRL
+    ppo_cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+    ppo_cfg["state_preprocessor"] = RunningStandardScaler
+    ppo_cfg["state_preprocessor_kwargs"] = {"size": env.observation_space}
+    ppo_cfg["value_preprocessor"] = RunningStandardScaler
+    ppo_cfg["value_preprocessor_kwargs"] = {"size": 1}
 
-    agent = PPO(
-        models=models,
-        memory=memory,
-        cfg=cfg,
+    ppo_agent = PPO(
+        models=ppo_models,
+        memory=ppo_memory,
+        cfg=ppo_cfg,
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+    )
+
+    sac_memory = RandomMemory(memory_size=1000000, num_envs=1)
+    sac_models = {}
+    sac_models["policy"] = Policy_Model(env.observation_space, env.action_space)
+    sac_models["critic_1"] = Critic_Model(env.observation_space, env.action_space)
+    sac_models["critic_2"] = Critic_Model(env.observation_space, env.action_space)
+    sac_models["target_critic_1"] = Critic_Model(
+        env.observation_space, env.action_space
+    )
+    sac_models["target_critic_2"] = Critic_Model(
+        env.observation_space, env.action_space
+    )
+
+    for role, model in sac_models.items():
+        model.init_state_dict(role)
+
+    sac_cfg = SAC_DEFAULT_CONFIG.copy()
+    sac_cfg["batch_size"] = 512
+    sac_cfg["learning_starts"] = 10000
+    sac_cfg["random_steps"] = 5000
+    sac_cfg["actor_learning_rate"] = 1e-4
+    sac_cfg["critic_learning_rate"] = 1e-4
+    sac_cfg["entropy_learning_rate"] = 1e-4
+    sac_cfg["state_preprocessor"] = RunningStandardScaler
+    sac_cfg["state_preprocessor_kwargs"] = {"size": env.observation_space}
+
+    sac_agent = SAC(
+        models=sac_models,
+        memory=sac_memory,
+        cfg=sac_cfg,
         observation_space=env.observation_space,
         action_space=env.action_space,
     )
 
     # load the latest checkpoint (adjust the path as needed)
-    # agent.load("runs/25-05-02_03-22-44-582590_PPO/checkpoints/best_agent.pickle")
+    # sac_agent.load("runs/25-05-10_01-02-59-821787_SAC/checkpoints/agent_500000.pickle")
 
     # configure and instantiate the RL trainer
     cfg_trainer = {"timesteps": 1000000, "headless": True}
-    trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[agent])
+    trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[ppo_agent])
 
     # start training
     trainer.train()
